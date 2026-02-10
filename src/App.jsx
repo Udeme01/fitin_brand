4a8fbc1ff47c2f1a69827329edb38f21e25bbaa0
Udeme01 <emmanueluddy2@gmail.com>
2026-02-10 15:37:32 +0100

    package-upgrade: tailwindcss upgraded to latest version

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
-  return <div>app component.</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <Homepage />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Homepage = () => {
+  return <div className="h-screen border">Homepage</div>;
+};
+
+export default Homepage;
