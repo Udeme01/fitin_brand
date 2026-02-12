13ffeab260aff56c85968e99219d9bd3d8d89436
Udeme01 <emmanueluddy2@gmail.com>
2026-02-12 09:50:31 +0100

    style adjustment on header

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -4,9 +4,9 @@ import Typography from "../ui/Typography";
 
 const Hero = () => {
   return (
-    <main className="h-[85vh] bg-[url('/images/products/wedding_dress_01.avif')] text-white bg-cover bg-no-repeat bg-center relative">
+    <main className="h-[90vh] bg-[url('/images/products/wedding_dress_01.avif')] text-white bg-cover bg-no-repeat bg-center relative">
       <div className="bg-black/20 absolute top-0 right-0 bottom-0 left-0" />
-      <section className="absolute bottom-10 right-5 space-y-4 md:top-1/2 md:right-1/2 md:translate-x-1/2 md:translate-y-1/2 md:text-center">
+      <section className="absolute bottom-20 right-5 space-y-4 md:top-1/2 md:right-1/2 md:translate-x-1/2 md:translate-y-1/2 md:text-center">
         <Typography variant="h2">discover</Typography>
         <Typography variant="h1">wedding collection</Typography>
 
