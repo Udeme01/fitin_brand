a3c29c1a370b7ba9511d348a8dc8d22e32f10e44
Udeme01 <emmanueluddy2@gmail.com>
2026-02-14 09:15:58 +0100

    worked on the contact's hero section and added contact details. working on contact form currently

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import contactimg from "/images/hero/wedding_dress_15.avif";
+import Input from "../components/ui/Input";
 
 const Contact = () => {
-  return <div>
-    contact
-  </div>;
+  return (
+    <section>
+      <section className="relative">
+        <img
+          src={contactimg}
+          alt="a woman wearing a white wedding gown"
+          className="w-full mx-auto object-cover h-[60vh]"
+        />
+        <div className="bg-black/20 absolute top-0 right-0 bottom-0 left-0" />
+        <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-5xl w-full text-center py-6">
+          Contact Us
+        </h1>
+      </section>
+
+      {/* contact details */}
+      <section className="flex flex-col items-center justify-center leading-7 my-8 space-y-4 font-light text-sm">
+        <div>
+          <p>3 Bello Cresent</p>
+          <p>Ilupeju, Lagos 102215</p>
+          <p>Lagos, Nigeria</p>
+        </div>
+        <div>
+          <label className="mr-2">Phone:</label>
+          <a
+            href="tel:+2347046780531"
+            className="underline font-light tracking-wider"
+          >
+            +2347046780531
+          </a>
+        </div>
+      </section>
+
+      {/* contact input fields */}
+      {/* there's a contact form component for this... */}
+    </section>
+  );
 };
 
 export default Contact;
